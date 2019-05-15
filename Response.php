f7715f7f8322d647531b7e5aca6f5273af738cf8
Andrés D. Molins <andres.molins@sddbrandcare.com>
2019-05-15 10:34:01 +0200

    Changed method to allow working with 5.2 version of the class

--- Response.php
+++ Response.php
@@ -21,19 +21,26 @@ class Response extends ResponseSymfony
         if (headers_sent()) {
             return $this;
         }
+
+        if (!$this->headers->has('Date')) {
+            $this->setDate(\DateTime::createFromFormat('U', time()));
+        }
+
         // headers
-        foreach ($this->headers->allPreserveCaseWithoutCookies() as $name => $values) {
-            $replace = 0 === strcasecmp($name, 'Content-Type');
+        foreach ($this->headers->allPreserveCase() as $name => $values) {
             foreach ($values as $value) {
-                header($name.': '.$value, $replace, $this->statusCode);
+                header($name.': '.$value, false, $this->statusCode);
             }
         }
+
         // cookies
         foreach ($this->headers->getCookies() as $cookie) {
             header('Set-Cookie: '.$cookie->getName().strstr($cookie, '='), false, $this->statusCode);
         }
+
         // status
         header(sprintf('HTTP/%s %s %s', $this->version, $this->statusCode, $this->statusText), true, $this->statusCode);
+
         return $this;
     }
 }
